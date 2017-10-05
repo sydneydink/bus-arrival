56ddde686ec5f4148e331ccb854f56e9b268f173
sydney <yongjing@dink.com.sg>
2017-10-05 17:26:43 +0800

    added time-stamp

--- app/models/participant.js
+++ app/models/participant.js
@@ -2,7 +2,15 @@ var mongoose = require('mongoose');
  
 var ParticipantSchema = new mongoose.Schema({ 	
   email: {type: String, unique: false}, 
-  recipe: String
+  recipe: String,
+  time: Date,
 });
+
+ParticipantSchema.pre('save', function(next){
+	var now = new Date();
+	this.time = now;
+	next();
+})
+
 
 mongoose.model("Participant", ParticipantSchema);
